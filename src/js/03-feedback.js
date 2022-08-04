14cdcb33c094094d9e130516d10b1f3b32ea9abd
Anton-Starshinov <97900625+Anton-Starshinov@users.noreply.github.com>
2022-08-04 19:05:20 +0300

    finish

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,34 +3,32 @@ import '../css/03-feedback.css';
 import throttle from 'lodash.throttle';
 
 const STORAGE_MAIL_KEY = 'feedback-form-state';
-pickUpFrom();
 const fromData = {};
 
-const refs = {
-  form: document.querySelector('.feedback-form'),
-  input: document.querySelector('.feedback-form input'),
-  textarea: document.querySelector('.feedback-form textarea'),
-};
+const formEl = document.querySelector('.feedback-form');
 
-refs.form.addEventListener('input', e => {
+formEl.addEventListener('input', throttle(onFormStorage, 500));
+formEl.addEventListener('submit', onFormSubmit);
+pickUpFrom();
+
+function onFormStorage(e) {
   fromData[e.target.name] = e.target.value;
 
   localStorage.setItem(STORAGE_MAIL_KEY, JSON.stringify(fromData));
-});
-
-refs.form.addEventListener('submit', onFormSubmit);
+}
 
 function onFormSubmit(e) {
   e.preventDefault();
 
   e.currentTarget.reset();
+  localStorage.removeItem(STORAGE_MAIL_KEY);
 }
 
 function pickUpFrom() {
   const textStorage = JSON.parse(localStorage.getItem(STORAGE_MAIL_KEY));
 
   if (textStorage) {
-    refs.input.value = textStorage.email;
-    refs.textarea.value = textStorage.message;
+    formEl.email.value = textStorage.email;
+    formEl.message.value = textStorage.message;
   }
 }
